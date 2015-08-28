8e79cf6600c9b95ebacedf7f48ecdfe762af075d
Anaminus <Anaminus@users.noreply.github.com>
2015-08-27 22:52:22 -0500

    Implement flag system.
    Split main function into shell and GUI.

--- main.go
+++ main.go
@@ -9,22 +9,57 @@ import (
 	"github.com/google/gxui/themes/dark"
 )
 
-func appMain(driver gxui.Driver) {
+var Option struct {
+	Debug        bool
+	SettingsFile string
+	UpdateData   bool
+	Shell        bool
+	OutputFile   string
+	OutputFormat string
+	New          bool
+	InputFile    string
+}
+
+func shellMain() {
+}
+
+func guiMain(driver gxui.Driver) {
 	GenerateIconTextures(driver)
 
 	theme := dark.CreateTheme(driver)
-	window := theme.CreateWindow(800, 600, "RBXplore")
+	window := theme.CreateWindow(800, 600, "rbxplore")
 
 	editor := &EditorContext{
 		session: &Session{},
 	}
-	ctxc, ok := CreateContextController(driver, window, theme, editor)
-	fmt.Println("CTXC", ctxc, ok)
+	CreateContextController(driver, window, theme, editor)
 
 	window.OnClose(driver.Terminate)
 	window.SetPadding(math.Spacing{L: 10, T: 10, R: 10, B: 10})
 }
 
 func main() {
-	gl.StartDriver(appMain)
+	flag.BoolVar(&Option.Debug, "debug", false, "Output debug messages to stdout.")
+	InitDebug()
+
+	flag.StringVar(&Option.SettingsFile, "settings", "", "Read and write settings from `file`. If unspecified, 'rbxplore-settings.json' is read/written from the same location as the executable.")
+	flag.BoolVar(&Option.UpdateData, "updatedata", false, "Update ReflectionMetadata and API dump files.")
+	flag.BoolVar(&Option.Shell, "shell", false, "Runs the program without a GUI.")
+	flag.StringVar(&Option.OutputFile, "output", "", "If --shell is true, export the input file to the given location. The format will be detected from the extension.")
+	flag.StringVar(&Option.OutputFormat, "format", "", "If --shell is true, export the input file with the given format. This overrides the output file extension. Valid formats are 'rbxl', 'rbxm', 'rbxlx', 'rbxmx', and 'json'. '_min' may be appended to output in a minimized format, if applicable.")
+	flag.BoolVar(&Option.New, "new", true, "If running with a GUI, force a new session to be opened.")
+	flag.Parse()
+	Option.InputFile = flag.Arg(0)
+
+	InitSettings()
+	Settings.SetFile(Option.SettingsFile)
+	Settings.Load()
+
+	InitData()
+
+	if Option.Shell {
+		shellMain()
+	} else {
+		gl.StartDriver(guiMain)
+	}
 }
